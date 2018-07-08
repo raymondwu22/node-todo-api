6c6d2f0838ca22312cd5cb41fd243e4b673f2f08
Rootventure <raywupharmd@gmail.com>
2018-07-07 23:28:08 -0400

    add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -127,6 +127,17 @@ app.post('/users/login', (req, res) => {
     .catch(e => res.status(400).send());
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user
+    .removeToken(req.token)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch(() => {
+      res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
   console.log(`App is running on PORT ${port}`);
 });
